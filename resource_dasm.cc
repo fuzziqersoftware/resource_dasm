5a847d176555190752db0a40795e518f9e9866ec
Martin Michelsen <fuzziqersoftware+g@gmail.com>
2021-12-11 12:17:35 -0800

    add MADI handler

--- resource_dasm.cc
+++ resource_dasm.cc
@@ -932,6 +932,7 @@ static unordered_map<uint32_t, resource_decode_fn> type_to_decode_fn({
 static const unordered_map<uint32_t, const char*> type_to_ext({
   {RESOURCE_TYPE_icns, "icns"},
   {RESOURCE_TYPE_MADH, "madh"},
+  {RESOURCE_TYPE_MADI, "madi"},
   {RESOURCE_TYPE_MOOV, "mov"},
   {RESOURCE_TYPE_MooV, "mov"},
   {RESOURCE_TYPE_moov, "mov"},
