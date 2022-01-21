aeed1b3a9b082030ce51f0107924fcfa022d4a94
Martin Michelsen <fuzziqersoftware+g@gmail.com>
2022-01-20 23:22:19 -0800

    make realmz_dasm_all more useful

--- realmz_dasm_all.sh
+++ realmz_dasm_all.sh
@@ -3,17 +3,19 @@
 set -e
 set -x
 
+INPUT_DIR="$1"
+
 OUTPUT_DIR=realmz_dasm_all.out
 
 mkdir -p realmz_dasm_all.out
-./realmz_dasm "Data Files" "$OUTPUT_DIR/Global Data"
+./realmz_dasm "$INPUT_DIR/Data Files" "$OUTPUT_DIR/Global Data"
 
-ls Scenarios | while read scenario
+ls "$INPUT_DIR/Scenarios" | while read scenario
 do
-  if [ -d "Scenarios/$scenario" ]
+  if [ -d "$INPUT_DIR/Scenarios/$scenario" ]
   then
     rm -rf "$OUTPUT_DIR/$scenario"
     mkdir -p "$OUTPUT_DIR/$scenario"
-    ./realmz_dasm "Data Files" "Scenarios/$scenario" "$OUTPUT_DIR/$scenario"
+    ./realmz_dasm "$INPUT_DIR/Data Files" "$INPUT_DIR/Scenarios/$scenario" "$OUTPUT_DIR/$scenario"
   fi
 done
